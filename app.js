913823953f4c29d7916e39a1a0371d54afbe637b
Austin Wang <yyaustin@bu.edu>
2023-03-16 18:11:06 -0400

    Finish exercise 4.1 & 4.2

---
+++ app.js
@@ -0,0 +1,29 @@
+const config = require("./utils/config")
+const logger = require("./utils/logger")
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+const blogsRouter = require("./controllers/blogs")
+
+// setup database connection
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("MongoDB connection successful")
+  })
+  .catch((error) => {
+    logger.error("MongoDB connection error:", error)
+  })
+
+// setup routers and middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.static("build"))
+
+app.use("/api/blogs", blogsRouter)
+
+app.use("/", (req, res) => {
+  res.send("hello")
+})
+
+module.exports = app
